591fada4dd006ee7dfa4f35106b9be9302ae8bf5
kodeforyou <venky.satya.aws@gmail.com>
2020-10-30 08:38:14 +0530

    inital commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into Resources (lastname,firstname,role,skillset,salary) values('raja','m', 'lead', 'java', 200);
+insert into Resources (lastname,firstname,role,skillset,salary) values('ravi','p', 'qa', 'selenium', 120);
+insert into Resources (lastname,firstname,role,skillset,salary) values('roja','c', 'developer', 'java', 150);
+insert into Resources (lastname,firstname,role,skillset,salary) values('cnu','b', 'manager', 'pmp', 300);
+insert into Resources (lastname,firstname,role,skillset,salary) values('sita','p', 'pricipal arch', 'java,togaf', 300);
+insert into Resources (lastname,firstname,role,skillset,salary) values('jaya','l', 'lead eng', 'java,dotnet', 100);
+insert into Resources (lastname,firstname,role,skillset,salary) values('geeta','m', 'lead account', 'accounts', 200);
+insert into Resources (lastname,firstname,role,skillset,salary) values('sony','k', 'lead finance', 'hr finance', 300);
+insert into Resources (lastname,firstname,role,skillset,salary) values('babu','b', 'lead hr', 'hr resources', 200);
+commit;
+
